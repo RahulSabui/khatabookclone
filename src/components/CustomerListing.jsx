dc32035ede298afb2b586874ab0b2926f54a5870
Rahul <sabuirahul90@gmail.com>
2024-07-20 10:39:43 +0530

    working on list

--- src/components/CustomerListing.jsx
+++ src/components/CustomerListing.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import List from "./List";
 
 const customer = [
@@ -19,12 +20,40 @@ const customer = [
 
 export default function CustomerListing() {
   return (
-    <section>
-      <div className="container-xl lg:container my-5">
-        <div className="grid grid-cols-1 gap-4 place-content-center place-items-center">
+    <section className="flex items-center justify-center ">
+      <div className="container-xl lg:container">
+        {/* <div className="grid grid-cols-1 gap-4 place-content-center place-items-center">
           {customer.map((cus) => {
             return <List key={cus.id} {...cus} />;
           })}
+        </div> */}
+        <div className="flex justify-center mt-10">
+          <table className="table-fixed w-full max-w-2xl border-collapse border border-gray-400">
+            <thead>
+              <tr>
+                <th className="border border-gray-300 px-4 py-2">Song</th>
+                <th className="border border-gray-300 px-4 py-2">Artist</th>
+                <th className="border border-gray-300 px-4 py-2">Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border border-gray-300 px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td className="border border-gray-300 px-4 py-2">Malcolm Lockyer</td>
+                <td className="border border-gray-300 px-4 py-2">1961</td>
+              </tr>
+              <tr>
+                <td className="border border-gray-300 px-4 py-2">Witchy Woman</td>
+                <td className="border border-gray-300 px-4 py-2">The Eagles</td>
+                <td className="border border-gray-300 px-4 py-2">1972</td>
+              </tr>
+              <tr>
+                <td className="border border-gray-300 px-4 py-2">Shining Star</td>
+                <td className="border border-gray-300 px-4 py-2">Earth, Wind, and Fire</td>
+                <td className="border border-gray-300 px-4 py-2">1975</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </section>
